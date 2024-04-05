d9e3af021ef940175913f0799ce6a3b9a780419d
shum-panda <shium.m.r@gmail.com>
2024-04-05 11:09:10 +0200

    skip futher excersises, becouse of redundancy from other Lecture

--- basic-exercises-py3/list2.py
+++ basic-exercises-py3/list2.py
@@ -6,9 +6,10 @@
 # all adjacent == elements have been reduced to a single element,
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
-def remove_adjacent(nums):
-  # +++your code here+++
-  return
+def remove_adjacent(nums) -> list:
+    shorted_list=[]
+    
+    return shorted_list
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
